65a1756db1c6edc64548deaf6579e77dd496be89
isaacwide <isaac.aguilard@alumno.buap.mx>
2024-08-29 19:00:26 -0600

    agredo

--- complejos.py
+++ complejos.py
@@ -2,56 +2,49 @@ import math
 
 class complejos:
  
-    def __init__(self,modulo,argumento,parteReal,parteImg):
-        self.modulo=modulo
-        self.argumeto=argumento
+    def __init__(self,parteReal,parteImg):
+        super().__init__()
         self.parteReal=parteReal
         self.parteImg=parteImg
         
     #forma trigonometrica de un complejo 
-    def forma_trigonometrica(self, parteReal, parteImg):
-        #se le asignara uno pa no tener una division pot cero 
-        if(parteReal== 0 ):
-             parteReal=1
+    def forma_trigonometrica(self):
         #inicio de los if para transformar el complejo en su forma trigonnometrica 
         #if del primer cuadrante 
-        if(parteReal>=0 and parteImg >=0):
-
-            if(parteReal==0):
-                theta=(math.pi/2)
-                argumento=math.degrees(theta)
-            else:
-                theta=math.atan(parteImg/parteReal)
-                aux=math.degrees(theta)
-                argumento=aux
+        if(self.parteReal>0 and self.parteImg >=0):
+            theta=math.atan(self.parteImg/self.parteReal)
+            aux=math.degrees(theta)
+            argumento=aux
         #if del segundo cuadrante 
-        if(parteReal <= 0 and parteImg>= 0):
-             theta=math.atan(parteImg/parteReal)
-             argumento=math.degrees(math.pi-(theta*-1))
-        
+        elif(self.parteReal < 0 and self.parteImg >= 0):
+             theta=math.atan(self.parteImg/self.parteReal)
+             argumento=math.degrees(math.pi-(theta*-1)) 
         #if del tercer cuandrate 
-        if(parteReal<=0 and parteImg<=0 ):
-            theta=math.atan(parteImg/parteReal)
+        elif(self.parteReal < 0 and self.parteImg <=0 ):
+            theta=math.atan(self.parteImg/self.parteReal)
             argumento=math.degrees(math.pi+theta)
-        # del tercer cudranrte
-        if(parteReal >= 0 and parteImg <= 0):
-            if(parteReal== 1 ):
-                theta=(math.pi/2)
-                argumento=math.degrees((2*math.pi)-theta)
-                
-            else:
-                theta=math.atan(parteImg/parteReal)
+        # if cuarto cudranrte
+        elif(self.parteReal> 0 and self.parteImg <= 0):
+                theta=math.atan(self.parteImg/self.parteReal)
                 argumento=math.degrees(((2*math.pi)-(theta*-1)))
+        #een caso de que la parte real sea cero
+        elif(self.parteReal==0):
+                  if(self.parteImg>0):
+                       argumento=math.degrees((math.pi)/2)
+                  else:
+                       argumento=math.degrees(math.pi)
+        elif(self.parteReal==0 and self.parteImg==0):
+             print("retorno al inicio")               
         return argumento   
   
-    def raices_complejos(self, parteReal, parteImg, n):
+    def raices_complejos(self, n):
         #aqui van las raices complejas
         #aqui van los argumentos de cada complejos
-        modulo=math.sqrt(parteReal**2+parteImg**2)
-        arg=math.radians(self.forma_trigonometrica(parteReal, parteImg))
+        modulo=math.sqrt(self.parteReal**2+self.parteImg**2)
+        arg=math.radians(self.forma_trigonometrica())
         lista_argumentos=[]
        #obtenemos el valor de deel agumento interno 
-        for i in range(0,n-1):
+        for i in range(0,n):
             argumento_complejo= (arg+ (2*math.pi)*i)/n
             lista_argumentos.append(argumento_complejo)
 
@@ -60,8 +53,10 @@ class complejos:
         return raiz_enecima_modulo , lista_argumentos
 
     
-num=complejos.forma_trigonometrica(2,2)
-num3,ew=complejos.raices_complejos(2,2,5)
 
-print(num)
-        
+numer=complejos(parteReal=2, parteImg=2)
+
+print(numer.forma_trigonometrica())
+
+# Obtener las raíces complejas
+print(numer.raices_complejos(3))
